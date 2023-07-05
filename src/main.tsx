42676de9e6e2f9e3a21ce39c46f790a37a1fbaa9
Matheus <matheusalmeida016@gmail.com>
2023-07-05 19:42:33 -0400

    feat: add prettier and improve main

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
-import { App } from "@/app";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { App } from '@/app'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 
-const rootElement = document.querySelector('[data-js="root"]') as HTMLElement;
+const rootElement = document.querySelector('[data-js="root"]') as HTMLElement
 
 if (!rootElement) {
-  throw new Error("Failed to find the root element");
+  throw new Error('Failed to find the root element')
 }
 
-const root = createRoot(rootElement);
+const root = createRoot(rootElement)
 
 root.render(
   <StrictMode>
     <App />
   </StrictMode>
-);
+)
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,10 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import { EsLinter, linterPlugin } from 'vite-plugin-linter'
 import viteSvgr from 'vite-plugin-svgr'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
-export default defineConfig(configEnv => ({
+export default defineConfig({
   base: '/',
-  plugins: [
-    react(),
-    tsConfigPaths(),
-    viteSvgr(),
-    linterPlugin({
-      include: ['./src/**/*.{ts,tsx}'],
-      linters: [new EsLinter({ configEnv })]
-    })
-  ],
-}))
+  plugins: [react(), tsConfigPaths(), viteSvgr()],
+})
