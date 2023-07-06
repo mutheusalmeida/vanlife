6bdb39ba15ba68820d71ff9f7ef526457bc6fe64
Matheus <matheusalmeida016@gmail.com>
2023-07-06 15:57:56 -0400

    feat: home component and husky

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,5 @@
+import { Home } from '@/home'
+
 export function App() {
-  return <>Hello, world!</>
+  return <Home />
 }
---
+++ src/home/home.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+export const Home = () => {
+  return (
+    <S.HomeWrapper>
+      <h1>home</h1>
+    </S.HomeWrapper>
+  )
+}
