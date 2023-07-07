e75d5b2862e5f54c44d93b57e15483b357a2c290
Matheus <matheusalmeida016@gmail.com>
2023-07-07 16:03:46 -0400

    fix: remove tailwind plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,6 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,12 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom'
 
-import * as S from "./styles";
+import * as S from './styles'
 
 export const Layout = () => {
   return (
     <S.LayoutWrapper className="text-1xl container">
       <h1 className="font-bold underline">layout</h1>
+
       <Outlet />
     </S.LayoutWrapper>
-  );
-};
+  )
+}
