886b80b73fb198b041af620e817741906c140959
Matheus <matheusalmeida016@gmail.com>
2023-07-07 15:11:03 -0400

    feat; add tailwind with prettier

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,13 +1,12 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet } from "react-router-dom";
 
-import * as S from './styles'
+import * as S from "./styles";
 
 export const Layout = () => {
   return (
-    <S.LayoutWrapper>
-      <h1 className="font-bold text-1xl">layout</h1>
-
+    <S.LayoutWrapper className="text-1xl container">
+      <h1 className="font-bold underline">layout</h1>
       <Outlet />
     </S.LayoutWrapper>
-  )
-}
+  );
+};
