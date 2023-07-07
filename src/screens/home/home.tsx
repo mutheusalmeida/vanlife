957af4bb9ac5a4a5ae1b196114aea553756ff4b4
Matheus <matheusalmeida016@gmail.com>
2023-07-07 19:14:07 -0400

    improve: add hero accessibility

--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 export const Home = () => {
   return (
-    <div className="bg-[url('../assets/hero-image.png')] bg-black-100 bg-blend-overlay bg-center bg-cover flex justify-center mx-auto">
+    <section aria-label="Main content" className="bg-[url('../assets/hero-image.png')] bg-black-100 bg-blend-overlay bg-center bg-cover flex justify-center mx-auto">
       <div className="max-w-lg flex flex-col px-7 py-16 gap-12">
         <div className="flex flex-col gap-6">
           <h2 className="text-white text-4xl leading-[42px] font-extrabold">You got the travel plans, we got the travel vans.</h2>
@@ -12,6 +12,6 @@ export const Home = () => {
 
         <Link className="font-bold bg-orange px-4 h-[50px] flex justify-center rounded-md leading-[50px] text-white" to="/vans">Find your van</Link>
       </div>
-    </div>
+    </section>
   )
 }
