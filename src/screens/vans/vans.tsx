e1e45c1a8180afba4a2c7ec8e159907950d67c17
Matheus <matheusalmeida016@gmail.com>
2023-07-10 17:57:44 -0400

    chore: list vans

--- src/screens/vans/vans.tsx
+++ src/screens/vans/vans.tsx
@@ -1,20 +1,35 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+
+type Data = {
+  id: string
+  name: string
+  price: number
+  description: string
+  imageUrl: string
+  type: string
+}
 
 export const Vans = () => {
+  const [data, setData] = useState<Data[] | null>(null)
+
   useEffect(() => {
     const getData = async () => {
       const res = await fetch('/api/vans')
       const data = await res.json()
 
-      console.log(data)
+      setData(data?.vans)
     }
 
     getData()
   }, [])
 
+  console.log(data)
+
   return (
     <div>
-      <h1>vans</h1>
+      {data?.map((van) => (
+        <div key={van.id}>{van.name}</div>
+      ))}
     </div>
   )
 }
