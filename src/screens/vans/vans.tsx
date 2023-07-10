48a9c33842f3ae1034312a9e4df26d8f5bdcb131
Matheus <matheusalmeida016@gmail.com>
2023-07-10 17:42:36 -0400

    feat: add mock data with miragejs

--- src/screens/vans/vans.tsx
+++ src/screens/vans/vans.tsx
@@ -1,4 +1,17 @@
+import { useEffect } from 'react'
+
 export const Vans = () => {
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch('/api/vans')
+      const data = await res.json()
+
+      console.log(data)
+    }
+
+    getData()
+  }, [])
+
   return (
     <div>
       <h1>vans</h1>
