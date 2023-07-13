5394cb3b7e27bff5f7e46aa3f9c8dec3d3223104
Matheus <matheusalmeida016@gmail.com>
2023-07-12 22:38:34 -0400

    feat: add active nav link

--- src/header/header.tsx
+++ src/header/header.tsx
@@ -1,18 +1,44 @@
-import { Link } from "react-router-dom"
+import { Link, NavLink } from 'react-router-dom'
+import { twMerge } from 'tailwind-merge'
 
 export const Header = () => {
   return (
     <div className="container flex justify-between items-center mx-auto py-7 px-8">
-      <h1 className="uppercase font-black text-black text-2xl" aria-label="VanLife logo">
-        <Link to="/">
-          #VanLife
-        </Link>
+      <h1
+        className="uppercase font-black text-black text-2xl"
+        aria-label="VanLife logo"
+      >
+        <Link to="/">#VanLife</Link>
       </h1>
 
-      <nav className="text-black-100 hover:text-black" aria-label="Main navigation">
+      <nav className="text-black-100" aria-label="Main navigation">
         <ul className="flex gap-6 font-semibold">
-          <li><Link className="hover:underline" to="/about">About</Link></li>
-          <li><Link className="hover:underline" to="/vans">Vans</Link></li>
+          <li>
+            <NavLink
+              className={({ isActive }) => {
+                return twMerge(
+                  'hover:text-black hover:underline',
+                  isActive ? 'text-black underline' : ''
+                )
+              }}
+              to="/about"
+            >
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={({ isActive }) => {
+                return twMerge(
+                  'hover:text-black hover:underline',
+                  isActive ? 'text-black underline' : ''
+                )
+              }}
+              to="/vans"
+            >
+              Vans
+            </NavLink>
+          </li>
         </ul>
       </nav>
     </div>
